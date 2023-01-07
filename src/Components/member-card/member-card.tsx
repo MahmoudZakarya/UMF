924bf95591720ccd4b37d21d8a4fc5f0ae8680c6
MahmoudZakaria <88841099+MahmoudZakarya@users.noreply.github.com>
2023-01-07 18:40:43 +0200

    modified team images

--- src/Components/member-card/member-card.tsx
+++ src/Components/member-card/member-card.tsx
@@ -7,10 +7,9 @@ export default function TeamCard({id,img_path, mName,
         , mP:string, twitter:string, insta:string
      }) {
          return (<> <div id={id} className="family-card">
-        <img className="member-img" src={img_path}  alt=""/>
+        <img className="member-img" src={process.env.PUBLIC_URL + img_path}  alt=""/>
         <h3 className="member-name">{mName}</h3>
         <h4 className="member-description">{mDescription}</h4>
-        <p className="member-p">{mP}</p>
            <div className="social-icons" id="member-social">
         <a className="icon" href={twitter} target='_blank' rel="noreferrer">
           <i className="fa-brands fa-twitter"></i>
